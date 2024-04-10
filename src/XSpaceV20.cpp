73425a361ad1c9ef9b3aa8339884a7fdfd2a3668
TheXSpacer <p.cardenasc@pucp.pe>
2024-04-10 15:43:30 -0500

    updated 100424

--- src/XSpaceV20.cpp
+++ src/XSpaceV20.cpp
@@ -1,9 +1,9 @@
 /*
   XSpace V20 Library
 
-  Version   :  1.0.2
+  Version   :  1.0.3
   Autor     :  Pablo Cardenas
-  Fecha     :  09/04/2024
+  Fecha     :  10/04/2024
 
   This is an open source library but dont remeber to reference!
 
@@ -11,70 +11,110 @@
 
 #include <XSpaceV20.h>
 
-volatile double TimerValue = 0;
-volatile double Periodo = 1000000;
-volatile double Tant = 0;
-volatile double counter = 0;
-volatile double Xval;
+volatile double TimerValue[2] = {0, 0};
+volatile double Periodo[2] = {1000000, 1000000};
+volatile double Tant[2] = {0, 0};
+volatile double counter[2] = {0, 0};
+volatile double Xval[2];
+XSEncoder encoder[2];
 
 WiFiClient XSpaceV2WifiClient;
 PubSubClient XSpaceV2MQTT(XSpaceV2WifiClient);
 void Mqtt_Callback(char* topic, byte* payload, unsigned int length);
 
-void IRAM_ATTR ISR_encoder(){
-    TimerValue = micros();
-    Xval = (-1+digitalRead(encoder_CHB)*2);
-    Periodo = (TimerValue - Tant)*Xval;
-    Tant = TimerValue;
-    counter = counter+Xval;
+void IRAM_ATTR ISR_encoder1(){
+    int idx = 0; // Índice para el primer encoder
+    TimerValue[idx] = micros();
+    Xval[idx] = (-1+digitalRead(encoder[idx].channelB)*2); // Asumiendo que encoder_CHB es global o estática
+    Periodo[idx] = (TimerValue[idx] - Tant[idx])*Xval[idx];
+    Tant[idx] = TimerValue[idx];
+    counter[idx] = counter[idx]+Xval[idx];
 }
 
-/*****************************XSpaceV2Board******************************************/
+void IRAM_ATTR ISR_encoder2(){
+    int idx = 1; // Índice para el primer encoder
+    TimerValue[idx] = micros();
+    Xval[idx] = (-1+digitalRead(encoder[idx].channelB)*2); // Asumiendo que encoder_CHB es global o estática
+    Periodo[idx] = (TimerValue[idx] - Tant[idx])*Xval[idx];
+    Tant[idx] = TimerValue[idx];
+    counter[idx] = counter[idx]+Xval[idx];
+}
 
-void XSpaceV20Board::init(int frequency, double resolution, double vm){
-    pinMode(nSLEEP,OUTPUT);
 
-    ledcSetup(1, frequency, 10);
-    ledcSetup(2, frequency, 10);
+/*****************************XSpaceV20Board******************************************/
 
-    ledcAttachPin(IN1, 1);
-    ledcAttachPin(IN2, 2);
+void XSpaceV20Board::init(int frequency, double resolution, double Vm){
+    this->DRV8837_init(DRV8837_IN1, 1, DRV8837_IN2, 2, DRV8837_nSLEEP, 20000, Vm);
+    this->encoder1_init(encoder_CHA, encoder_CHB, resolution, RISING);
+    this->_encoder_idx = 0;
+}
 
-    pinMode(encoder_CHA,INPUT_PULLUP);
-    pinMode(encoder_CHB,INPUT_PULLUP);
-    attachInterrupt(encoder_CHA, ISR_encoder, HIGH);
+void XSpaceV20Board::encoder1_init(int enc_CHAx, int enc_CHBx, int resolutionx, int mode){
+    
+    encoder[0].channelA = enc_CHAx;
+    encoder[0].channelB = enc_CHBx;
+    encoder[0].resolution = resolutionx;
 
-    this->_resolution = resolution;
-    this->_vm = vm;
+    pinMode(encoder[0].channelA, INPUT_PULLUP);
+    pinMode(encoder[0].channelB, INPUT_PULLUP);
+    attachInterrupt(encoder[0].channelA, ISR_encoder1, mode);
+}
+void XSpaceV20Board::encoder2_init(int enc_CHAx, int enc_CHBx, int resolutionx, int mode){
+    encoder[1].channelA = enc_CHAx;
+    encoder[1].channelB = enc_CHBx;
+    encoder[1].resolution = resolutionx;
+
+    pinMode(encoder[1].channelA, INPUT_PULLUP);
+    pinMode(encoder[1].channelB, INPUT_PULLUP);
+    attachInterrupt(encoder[1].channelA, ISR_encoder2, mode);
+}
+
+void XSpaceV20Board::DRV8837_init(int IN1x, int CH_IN1x, int IN2x, int CH_IN2x, int nSLEEPx,int frequencyx, int Vmx){
+    DRV8837.IN1 = IN1x;
+    DRV8837.IN2 = IN2x;
+    DRV8837.nSLEEP = nSLEEPx;
+    DRV8837.PWM_CH_IN1 = CH_IN1x;
+    DRV8837.PWM_CH_IN2 = CH_IN2x;
+    DRV8837.Vm = Vmx;
+
+    pinMode(DRV8837.nSLEEP,OUTPUT);
+    this->DRV8837_Sleep();
+
+    ledcSetup(DRV8837.PWM_CH_IN1, frequencyx, 10);
+    ledcSetup(DRV8837.PWM_CH_IN2, frequencyx, 10);
+
+    ledcAttachPin(DRV8837.IN1, DRV8837.PWM_CH_IN1);
+    ledcAttachPin(DRV8837.IN2, DRV8837.PWM_CH_IN2);
+
 }
 
 void XSpaceV20Board::DRV8837_Sleep(){
-    digitalWrite(nSLEEP,LOW);
+    digitalWrite(DRV8837.nSLEEP,LOW);
 }
 
 void XSpaceV20Board::DRV8837_Wake(){
-    digitalWrite(nSLEEP,HIGH);
+    digitalWrite(DRV8837.nSLEEP,HIGH);
 }
 
-void XSpaceV20Board::DRV8837_Voltage(double vp){
+void XSpaceV20Board::DRV8837_Voltage(double Vp){
 
-    if(vp>_vm) vp = _vm;
-    if(vp<-_vm) vp = -_vm;
-    uint32_t Duty = (uint32_t) ( (1 - abs(vp)/_vm) * 1024.0);
+    if(Vp>DRV8837.Vm) Vp = DRV8837.Vm;
+    if(Vp<-DRV8837.Vm) Vp = -DRV8837.Vm;
+    uint32_t Duty = (uint32_t) ( (1 - abs(Vp)/DRV8837.Vm) * 1023.0);
 
-    if(vp<0){
-        ledcWrite(1, Duty);
-        ledcWrite(2, 1024);
+    if(Vp<0){
+        ledcWrite(DRV8837.PWM_CH_IN1, Duty);
+        ledcWrite(DRV8837.PWM_CH_IN2, 1023);
     }else{
-        ledcWrite(1, 1024);
-        ledcWrite(2, Duty);
+        ledcWrite(DRV8837.PWM_CH_IN1, 1023);
+        ledcWrite(DRV8837.PWM_CH_IN2, Duty);
     }
 }
 
 double XSpaceV20Board::GetEncoderSpeed(int modo){
     double vel=0;
 
-    vel = 360000000.0/(_resolution*Periodo);
+    vel = 360000000.0/(encoder[_encoder_idx].resolution*Periodo[_encoder_idx]);
     if(abs(vel)>600)vel=_vel_ant;
     if(abs(vel-_vel_ant)>600)vel=_vel_ant;
     _vel_ant = vel;
@@ -100,10 +140,10 @@ double XSpaceV20Board::GetEncoderPosition(int modo){
     switch (modo){
         
     case DEGREES:
-        pos = counter/this->_resolution*360.0;
+        pos = counter[_encoder_idx]/encoder[_encoder_idx].resolution*360.0;
         break;
     case RADS:
-        pos = counter/this->_resolution*2*PI;
+        pos = counter[_encoder_idx]/encoder[_encoder_idx].resolution*2*PI;
         break;
     
     default:
