c85f2bd70dab844072c2b7a52c31548a0e193272
TheXSpacer <p.cardenasc@pucp.pe>
2024-04-13 18:05:11 -0500

    updated 130424

--- include/XSpaceV20.h
+++ include/XSpaceV20.h
@@ -1,11 +1,11 @@
 /*
-  XSpace V20 Library
+	XSpace V20 Library
 
-  Version   :  1.0.3
-  Autor     :  Pablo Cardenas
-  Fecha     :  10/04/2024
+	Version   :  1.0.4
+	Autor     :  Pablo Cardenas
+	Fecha     :  12/04/2024
 
-  This is an open source library but dont remeber to reference!
+	This is an open source library but dont remeber to reference!
 
 */
 
@@ -23,6 +23,8 @@
 
 #define encoder_CHA 34
 #define encoder_CHB 35
+#define E1 0
+#define E2 1
 
 #define DEGREES_PER_SECOND 1
 #define RADS_PER_SECOND 2
@@ -32,55 +34,54 @@
 
 
 struct XSEncoder{
-  int resolution;
-  int channelA;
-  int channelB;
+	int resolution;
+	int channelA;
+	int channelB;
 };
 
 struct XSDRV8837{
-  int IN1;
-  int IN2;
-  int PWM_CH_IN1;
-  int PWM_CH_IN2;
-  int nSLEEP;
-  double Vm;
+	int IN1;
+	int IN2;
+	int PWM_CH_IN1;
+	int PWM_CH_IN2;
+	int nSLEEP;
+	double Vm;
 };
 
 class XSpaceV20Board{
-    private:
-        double _vel_ant = 0;
-        bool   _xspace_info = false;
-        int _encoder_idx;
+		private:
+				double _vel_ant = 0;
+				bool   _xspace_info = false;
 
-        XSDRV8837 DRV8837;
+				XSDRV8837 DRV8837;
 
-    public:
-        /*init method
-        frequency  => PWM frequency for drv8837 driver (Hz)
-        resolution => encoder pulses/rev * gearmotor reduction 
-        VM => DRV8837 Driver Power Supply voltage (Min=0v Max=11v)*/
-        void init(int frequency, double resolution, double VM);
+		public:
+				/*init method
+				frequency  => PWM frequency for drv8837 driver (Hz)
+				resolution => encoder pulses/rev * gearmotor reduction 
+				VM => DRV8837 Driver Power Supply voltage (Min=0v Max=11v)*/
+				void init(int frequency, double resolution, double VM);
 
-        void encoder1_init(int enc_CHAx, int enc_CHBx, int resolutionx, int mode);
-        void encoder2_init(int enc_CHAx, int enc_CHBx, int resolutionx, int mode);
+				void encoder1_init(int enc_CHAx, int enc_CHBx, int resolutionx, int mode);
+				void encoder2_init(int enc_CHAx, int enc_CHBx, int resolutionx, int mode);
 
-        void DRV8837_init(int IN1x, int CH_IN1x, int IN2x, int CH_IN2x, int nSLEEPx,int frequencyx, int Vmx);
-        void DRV8837_Sleep();
-        void DRV8837_Wake();
-        void DRV8837_Voltage(double vp);
-        
-        double GetEncoderSpeed(int modo);
-        double GetEncoderPosition(int modo);
+				void DRV8837_init(int IN1x, int CH_IN1x, int IN2x, int CH_IN2x, int nSLEEPx,int frequencyx, int Vmx);
+				void DRV8837_Sleep();
+				void DRV8837_Wake();
+				void DRV8837_Voltage(double vp);
+				
+				double GetEncoderSpeed(int encoder, int modo);
+				double GetEncoderPosition(int encoder, int modo);
 
-        void SerialInfo(bool mode);
-        void Wifi_init(const char* ssid, const char* password);
-        void Mqtt_init(const char* mqtt_server, uint16_t mqtt_port);
-        void Mqtt_Connect(const char *clientId, const char *mqtt_user, const char *mqtt_pass);
-        void Mqtt_Publish(const char* topic, const char* payload);
-        void Mqtt_Suscribe(const char* topic);
-        void Mqtt_KeepAlive();
-        
-        bool Mqtt_IsConnected();
+				void SerialInfo(bool mode);
+				void Wifi_init(const char* ssid, const char* password);
+				void Mqtt_init(const char* mqtt_server, uint16_t mqtt_port);
+				void Mqtt_Connect(const char *clientId, const char *mqtt_user, const char *mqtt_pass);
+				void Mqtt_Publish(const char* topic, const char* payload);
+				void Mqtt_Suscribe(const char* topic);
+				void Mqtt_KeepAlive();
+				
+				bool Mqtt_IsConnected();
 };
 
 
--- src/XSpaceV20.cpp
+++ src/XSpaceV20.cpp
@@ -1,9 +1,9 @@
 /*
   XSpace V20 Library
 
-  Version   :  1.0.3
+  Version   :  1.0.4
   Autor     :  Pablo Cardenas
-  Fecha     :  10/04/2024
+  Fecha     :  12/04/2024
 
   This is an open source library but dont remeber to reference!
 
@@ -23,145 +23,141 @@ PubSubClient XSpaceV2MQTT(XSpaceV2WifiClient);
 void Mqtt_Callback(char* topic, byte* payload, unsigned int length);
 
 void IRAM_ATTR ISR_encoder1(){
-    int idx = 0; // Índice para el primer encoder
-    TimerValue[idx] = micros();
-    Xval[idx] = (-1+digitalRead(encoder[idx].channelB)*2); // Asumiendo que encoder_CHB es global o estática
-    Periodo[idx] = (TimerValue[idx] - Tant[idx])*Xval[idx];
-    Tant[idx] = TimerValue[idx];
-    counter[idx] = counter[idx]+Xval[idx];
+	TimerValue[E1] = micros();
+	Xval[E1] = (-1+digitalRead(encoder[E1].channelB)*2); // Asumiendo que encoder_CHB es global o estática
+	Periodo[E1] = (TimerValue[E1] - Tant[E1])*Xval[E1];
+	Tant[E1] = TimerValue[E1];
+	counter[E1] = counter[E1]+Xval[E1];
 }
 
 void IRAM_ATTR ISR_encoder2(){
-    int idx = 1; // Índice para el primer encoder
-    TimerValue[idx] = micros();
-    Xval[idx] = (-1+digitalRead(encoder[idx].channelB)*2); // Asumiendo que encoder_CHB es global o estática
-    Periodo[idx] = (TimerValue[idx] - Tant[idx])*Xval[idx];
-    Tant[idx] = TimerValue[idx];
-    counter[idx] = counter[idx]+Xval[idx];
+	TimerValue[E2] = micros();
+	Xval[E2] = (-1+digitalRead(encoder[E2].channelB)*2); // Asumiendo que encoder_CHB es global o estática
+	Periodo[E2] = (TimerValue[E2] - Tant[E2])*Xval[E2];
+	Tant[E2] = TimerValue[E2];
+	counter[E2] = counter[E2]+Xval[E2];
 }
 
 
 /*****************************XSpaceV20Board******************************************/
 
 void XSpaceV20Board::init(int frequency, double resolution, double Vm){
-    this->DRV8837_init(DRV8837_IN1, 1, DRV8837_IN2, 2, DRV8837_nSLEEP, 20000, Vm);
-    this->encoder1_init(encoder_CHA, encoder_CHB, resolution, RISING);
-    this->_encoder_idx = 0;
+	this->DRV8837_init(DRV8837_IN1, 1, DRV8837_IN2, 2, DRV8837_nSLEEP, 20000, Vm);
+	this->encoder1_init(encoder_CHA, encoder_CHB, resolution, RISING);
 }
 
-void XSpaceV20Board::encoder1_init(int enc_CHAx, int enc_CHBx, int resolutionx, int mode){
-    
-    encoder[0].channelA = enc_CHAx;
-    encoder[0].channelB = enc_CHBx;
-    encoder[0].resolution = resolutionx;
+void XSpaceV20Board::encoder1_init(int enc_CHAx, int enc_CHBx, int resolutionx, int mode){	
+	encoder[E1].channelA = enc_CHAx;
+	encoder[E1].channelB = enc_CHBx;
+	encoder[E1].resolution = resolutionx;
 
-    pinMode(encoder[0].channelA, INPUT_PULLUP);
-    pinMode(encoder[0].channelB, INPUT_PULLUP);
-    attachInterrupt(encoder[0].channelA, ISR_encoder1, mode);
+	pinMode(encoder[E1].channelA, INPUT_PULLUP);
+	pinMode(encoder[E1].channelB, INPUT_PULLUP);
+	attachInterrupt(encoder[E1].channelA, ISR_encoder1, mode);
 }
 void XSpaceV20Board::encoder2_init(int enc_CHAx, int enc_CHBx, int resolutionx, int mode){
-    encoder[1].channelA = enc_CHAx;
-    encoder[1].channelB = enc_CHBx;
-    encoder[1].resolution = resolutionx;
+	encoder[E2].channelA = enc_CHAx;
+	encoder[E2].channelB = enc_CHBx;
+	encoder[E2].resolution = resolutionx;
 
-    pinMode(encoder[1].channelA, INPUT_PULLUP);
-    pinMode(encoder[1].channelB, INPUT_PULLUP);
-    attachInterrupt(encoder[1].channelA, ISR_encoder2, mode);
+	pinMode(encoder[E2].channelA, INPUT_PULLUP);
+	pinMode(encoder[E2].channelB, INPUT_PULLUP);
+	attachInterrupt(encoder[E2].channelA, ISR_encoder2, mode);
 }
 
 void XSpaceV20Board::DRV8837_init(int IN1x, int CH_IN1x, int IN2x, int CH_IN2x, int nSLEEPx,int frequencyx, int Vmx){
-    DRV8837.IN1 = IN1x;
-    DRV8837.IN2 = IN2x;
-    DRV8837.nSLEEP = nSLEEPx;
-    DRV8837.PWM_CH_IN1 = CH_IN1x;
-    DRV8837.PWM_CH_IN2 = CH_IN2x;
-    DRV8837.Vm = Vmx;
+	DRV8837.IN1 = IN1x;
+	DRV8837.IN2 = IN2x;
+	DRV8837.nSLEEP = nSLEEPx;
+	DRV8837.PWM_CH_IN1 = CH_IN1x;
+	DRV8837.PWM_CH_IN2 = CH_IN2x;
+	DRV8837.Vm = Vmx;
 
-    pinMode(DRV8837.nSLEEP,OUTPUT);
-    this->DRV8837_Sleep();
+	pinMode(DRV8837.nSLEEP,OUTPUT);
+	this->DRV8837_Sleep();
 
-    ledcSetup(DRV8837.PWM_CH_IN1, frequencyx, 10);
-    ledcSetup(DRV8837.PWM_CH_IN2, frequencyx, 10);
+	ledcSetup(DRV8837.PWM_CH_IN1, frequencyx, 10);
+	ledcSetup(DRV8837.PWM_CH_IN2, frequencyx, 10);
 
-    ledcAttachPin(DRV8837.IN1, DRV8837.PWM_CH_IN1);
-    ledcAttachPin(DRV8837.IN2, DRV8837.PWM_CH_IN2);
+	ledcAttachPin(DRV8837.IN1, DRV8837.PWM_CH_IN1);
+	ledcAttachPin(DRV8837.IN2, DRV8837.PWM_CH_IN2);
 
 }
 
 void XSpaceV20Board::DRV8837_Sleep(){
-    digitalWrite(DRV8837.nSLEEP,LOW);
+	digitalWrite(DRV8837.nSLEEP,LOW);
 }
 
 void XSpaceV20Board::DRV8837_Wake(){
-    digitalWrite(DRV8837.nSLEEP,HIGH);
+	digitalWrite(DRV8837.nSLEEP,HIGH);
 }
 
 void XSpaceV20Board::DRV8837_Voltage(double Vp){
 
-    if(Vp>DRV8837.Vm) Vp = DRV8837.Vm;
-    if(Vp<-DRV8837.Vm) Vp = -DRV8837.Vm;
-    uint32_t Duty = (uint32_t) ( (1 - abs(Vp)/DRV8837.Vm) * 1023.0);
+	if(Vp>DRV8837.Vm) Vp = DRV8837.Vm;
+	if(Vp<-DRV8837.Vm) Vp = -DRV8837.Vm;
+	uint32_t Duty = (uint32_t) ( (1 - abs(Vp)/DRV8837.Vm) * 1023.0);
 
-    if(Vp<0){
-        ledcWrite(DRV8837.PWM_CH_IN1, Duty);
-        ledcWrite(DRV8837.PWM_CH_IN2, 1023);
-    }else{
-        ledcWrite(DRV8837.PWM_CH_IN1, 1023);
-        ledcWrite(DRV8837.PWM_CH_IN2, Duty);
-    }
+	if(Vp<0){
+		ledcWrite(DRV8837.PWM_CH_IN1, Duty);
+		ledcWrite(DRV8837.PWM_CH_IN2, 1023);
+	}else{
+		ledcWrite(DRV8837.PWM_CH_IN1, 1023);
+		ledcWrite(DRV8837.PWM_CH_IN2, Duty);
+	}
 }
 
-double XSpaceV20Board::GetEncoderSpeed(int modo){
-    double vel=0;
+double XSpaceV20Board::GetEncoderSpeed(int encx, int modo){
+	double vel=0;
 
-    vel = 360000000.0/(encoder[_encoder_idx].resolution*Periodo[_encoder_idx]);
-    if(abs(vel)>600)vel=_vel_ant;
-    if(abs(vel-_vel_ant)>600)vel=_vel_ant;
-    _vel_ant = vel;
+	vel = 360000000.0/(encoder[encx].resolution*Periodo[encx]);
+	if(abs(vel)>600)vel=_vel_ant;
+	if(abs(vel-_vel_ant)>600)vel=_vel_ant;
+	_vel_ant = vel;
 
-    switch (modo)
-    {
-    case DEGREES_PER_SECOND:
-        return vel;
-        break;
-    case RADS_PER_SECOND:
-        return vel*PI/180;
-        break;
-    
-    default:
-        break;
-    }
+	switch (modo)
+	{
+	case DEGREES_PER_SECOND:
+		return vel;
+		break;
+	case RADS_PER_SECOND:
+		return vel*PI/180;
+		break;
+	
+	default:
+		break;
+	}
 
-    return vel;
+	return vel;
 }
-double XSpaceV20Board::GetEncoderPosition(int modo){
-    double pos=0;
+double XSpaceV20Board::GetEncoderPosition(int encx, int modo){
+	double pos=0;
 
-    switch (modo){
-        
-    case DEGREES:
-        pos = counter[_encoder_idx]/encoder[_encoder_idx].resolution*360.0;
-        break;
-    case RADS:
-        pos = counter[_encoder_idx]/encoder[_encoder_idx].resolution*2*PI;
-        break;
-    
-    default:
-        break;
-    }
+	switch (modo){
+		
+	case DEGREES:
+		pos = counter[encx]/(double)encoder[encx].resolution*360.0;
+		break;
+	case RADS:
+		pos = counter[encx]/(double)encoder[encx].resolution*2*PI;
+		break;
+	
+	default:
+		break;
+	}
 
-    return pos;
+	return pos;
 }
 
 /* Metodos reescritos para un mejor entendimiento */
 
 void XSpaceV20Board::Wifi_init(const char* ssid, const char* password){
 	
-    if(this->_xspace_info){
-        Serial.println();
-	    Serial.print("Conectando a ssid: ");
-	    Serial.println(ssid);
-    }
+	if(this->_xspace_info){
+		Serial.println();
+		Serial.print("Conectando a ssid: ");
+		Serial.println(ssid);
+	}
 
 	WiFi.begin(ssid, password);
 
@@ -170,13 +166,13 @@ void XSpaceV20Board::Wifi_init(const char* ssid, const char* password){
 		if(this->_xspace_info) Serial.print(".");
 	}
 
-    if(this->_xspace_info){
-	    Serial.println("");
-	    Serial.println("Conectado!!");
-	    Serial.println("Dirección IP: ");
-	    Serial.println(WiFi.localIP());
-    }
-    WiFi.setSleep(false);
+	if(this->_xspace_info){
+		Serial.println("");
+		Serial.println("Conectado!!");
+		Serial.println("Dirección IP: ");
+		Serial.println(WiFi.localIP());
+	}
+	WiFi.setSleep(false);
 
 }
 
@@ -188,50 +184,50 @@ void XSpaceV20Board::Mqtt_Connect(const char *clientId, const char *mqtt_user, c
 		// Intentamos conectar
 		if (XSpaceV2MQTT.connect(clientId,mqtt_user,mqtt_pass)) {
 				if(this->_xspace_info) Serial.println("Conectado al Broker!");
-                
+				
 		}else {
 			if(this->_xspace_info){
-                Serial.print("falló :( con error -> ");
-			    Serial.print(XSpaceV2MQTT.state());
-			    Serial.println(" Intentamos de nuevo en 5 segundos");
-			    delay(5000);
-            }
+				Serial.print("falló :( con error -> ");
+				Serial.print(XSpaceV2MQTT.state());
+				Serial.println(" Intentamos de nuevo en 5 segundos");
+				delay(5000);
+			}
 		}
 	}
 
 }
 
 void XSpaceV20Board::Mqtt_init(const char *mqtt_server, uint16_t mqtt_port){
-    XSpaceV2MQTT.setServer(mqtt_server, mqtt_port);
-    XSpaceV2MQTT.setCallback([this] (char* topic, byte* payload, unsigned int length) { Mqtt_Callback(topic, payload, length); });
+	XSpaceV2MQTT.setServer(mqtt_server, mqtt_port);
+	XSpaceV2MQTT.setCallback([this] (char* topic, byte* payload, unsigned int length) { Mqtt_Callback(topic, payload, length); });
 }
 
 bool XSpaceV20Board::Mqtt_IsConnected(){
-    return XSpaceV2MQTT.connected();
+	return XSpaceV2MQTT.connected();
 }
 
 void XSpaceV20Board::Mqtt_Publish(const char* topic, const char* payload){
-    XSpaceV2MQTT.publish(topic,payload);
+	XSpaceV2MQTT.publish(topic,payload);
 }
 
 void XSpaceV20Board::Mqtt_Suscribe(const char* topic){
-    if(this->Mqtt_IsConnected()){
-         XSpaceV2MQTT.subscribe(topic);
-         if(this->_xspace_info){
-             Serial.print("Suscrito a: ");
-             Serial.println(topic);
-         }else{
-             Serial.println("Subscripcion fallida, no se tiene conexion con el broker");
-         }
-    }
+	if(this->Mqtt_IsConnected()){
+		 XSpaceV2MQTT.subscribe(topic);
+		 if(this->_xspace_info){
+			 Serial.print("Suscrito a: ");
+			 Serial.println(topic);
+		 }else{
+			 Serial.println("Subscripcion fallida, no se tiene conexion con el broker");
+		 }
+	}
    
 
 }
 
 void XSpaceV20Board::Mqtt_KeepAlive(){
-    XSpaceV2MQTT.loop();
+	XSpaceV2MQTT.loop();
 }
 
 void XSpaceV20Board::SerialInfo(bool mode){
-    this->_xspace_info = mode;
+	this->_xspace_info = mode;
 }
